b376bacf91de6bccd97a417a9a6b1bbfc3b208b3
ali076 <ali.hasnain@redsignal.biz>
2023-11-09 23:14:52 +0500

    working on fontawesome icon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,66 +7,52 @@ const Home = () => {
 
     const [pageNo, setPageNo] = useState(1);
     const [totalPage, setTotalPage] = useState('');
-    const [topicId, setTopicId]= useState('');
+    const [topicId, setTopicId] = useState('');
     const [topics, setTopics] = useState([]);
     const [issues, setIssues] = useState([]);
     const [topicName, setTopicName] = useState('Latest Issues');
     const [search, setSearch] = useState('');
-    const [nextPage,setNextPage]=useState(true);
+    const [nextPage, setNextPage] = useState(true);
     const [prevPage, setPrevPage] = useState(true);
 
     useEffect(() => {
-        document.title='Home';
+        document.title = 'Home';
         fetchTopics();
-        fetchIssues();
     }, []);
 
+    useEffect(() => {
+        fetchIssues();
+    }, [pageNo, topicId, search]);
+
     const fetchTopics = async () => {
-        await axios.get(base_url+'topics')
+        await axios.get(base_url + 'topics')
             .then(res => {
-                setTopics(res?.data.data);; 
-        }).catch(err => console.log(err));
-        
+                setTopics(res?.data.data);;
+            }).catch(err => console.log(err));
     }
 
-    const fetchIssues = async (id = '', name = 'Latest Issues', search = '') => {
-        // id ? setSearch('') : setSearch(search);
-        // id ? setPageNo('') : setPageNo(pageNo);
-        // id ? setTopicId(id) : ( setTopicId? setTopicId(topicId): setTopicId(''));
-        // setTopicId? (id ? setTopicId(id) : setTopicId('')) : setTopicId(''); 
-        // console.log(pageNo);
-        await axios.get(base_url+"issues?" + (id ? `?id=${id}` : '') + (search ? `&search=${search}` : "" ) + (pageNo >`` ? `&page=${pageNo}` : "" )).then(res => {
-            console.log(res);           
+    const fetchIssues = async () => {
+        await axios.get(base_url + "issues?" + (topicId ? `?id=${topicId}` : '') + (search ? `&search=${search}` : "") + (pageNo > 0 ? `&page=${pageNo}` : "")).then(res => {
+            console.log(res);
             res?.data?.data?.prev_page_url ? setPrevPage(true) : setPrevPage(false);
             res?.data?.data?.next_page_url ? setNextPage(true) : setNextPage(false);
-            // res?.data?.data?.current_page ? setPageNo(res?.data?.data?.current_page) : setPageNo('');
             res?.data?.data?.last_page ? setTotalPage(res?.data?.data?.last_page) : setTotalPage('');
             setIssues(res?.data?.data?.data);
         }).catch(err => console.log(err));
-
-         setTopicName(name??'Latest Issues')
     }
 
     const searchIssue = (search) => {
-        let name=search ?search:'Latest Issues';
-        fetchIssues('', name, search);
-    }
-    
-    
-    const incrementPage= ()=>{
-        let page=pageNo;
-        page = page + 1;
-        console.log('page ' + page);
-        setPageNo(page)
-        console.log(pageNo);
-        fetchIssues('',topicName,'');
+        let name = search ? search : 'Latest Issues';
+        setTopicName(name);
+        setPageNo(1);
+        setSearch(search);
     }
 
-    const decrementPage=()=>{
-        // let page=pageNo;
-        // page = page - 1;
-        setPageNo(pageNo -1)
-        fetchIssues('',topicName,'');
+    const changeTopic = (id, name) => {
+        setTopicName(name);
+        setTopicId(id);
+        setPageNo(1);
+        setSearch('');
     }
 
     return (
@@ -77,11 +63,11 @@ const Home = () => {
                     <ul className="text-lg ml-5">
                         {
                             topics.length > 0 ?
-                            topics.map((item, index) => (
-                                <li className="cursor-pointer text-gray-500" value={search} onClick={() => fetchIssues(item.id, item.name)} key={index}>{item.name}</li>
-                            ))       
-                            : <p className=" text-gray-400 ">No topics found</p>
-                         }
+                                topics.map((item, index) => (
+                                    <li className="cursor-pointer text-gray-500" onClick={() => changeTopic(item.id, item.name)} key={index}>{item.name}</li>
+                                ))
+                                : <p className=" text-gray-400 ">No topics found</p>
+                        }
                     </ul>
                 </div>
             </div>
@@ -91,7 +77,7 @@ const Home = () => {
                         <h1 className="text-2xl font-bold">Issues</h1>
                     </div>
                     <div>
-                        <input className="p-2 border border-gray-400" type="text" placeholder="Search" onKeyUp={(e)=>searchIssue(e.target.value)}/>
+                        <input className="p-2 border border-gray-400" type="text" placeholder="Search" onKeyUp={(e) => searchIssue(e.target.value)} />
                     </div>
                 </div>
                 <p className="mt-3 text-xl text-yellow-800 font-semibold">{topicName}</p>
@@ -100,23 +86,23 @@ const Home = () => {
                         {
                             issues.length > 0 ?
                                 issues.map((item, index) => (
-                                <Issue item={item} key={index}/>
-                            ))
-                            : <div className="text-2xl text-gray-400 ">No issues found</div>
+                                    <Issue item={item} key={index} />
+                                ))
+                                : <div className="text-2xl text-gray-400 ">No issues found</div>
                         }
                     </ul>
 
                     {
                         issues.length > 0 ?
-                         <div className="flex justify-center space-x-2 mt-5">
-                            <button className={ prevPage ? "border-gray-800 font-bold p-2 rounded border-solid border-2" : "p-2 rounded border-gray-400  border-solid border-2"} disabled={!prevPage}  onClick={()=>decrementPage()}>Previous</button>
-                                <span className='mt-2'> {pageNo} of { totalPage }</span>
-                            <button className={ nextPage ? "border-gray-700 font-bold p-2 rounded border-solid border-2 " : "p-2 rounded border-gray-400  border-solid border-2 ml-2"} disabled={!nextPage} onClick={()=>incrementPage()}>Next</button>
-                        </div> :"" 
+                            <div className="flex justify-center space-x-2 mt-5">
+                                <button className={prevPage ? "border-gray-800 font-bold p-2 rounded border-solid border-2" : "p-2 rounded border-gray-400  border-solid border-2"} disabled={!prevPage} onClick={() => setPageNo(pageNo - 1)}>Previous</button>
+                                <span className='mt-2'> {pageNo} of {totalPage}</span>
+                                <button className={nextPage ? "border-gray-700 font-bold p-2 rounded border-solid border-2 " : "p-2 rounded border-gray-400  border-solid border-2 ml-2"} disabled={!nextPage} onClick={async () => setPageNo(pageNo + 1)}>Next</button>
+                            </div> : ""
                     }
                 </div>
             </div>
-        </div>  
+        </div>
     )
 }
 
--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -2,7 +2,11 @@ const Issue = (props) => {
     return (
         <div className="mb-2 p-3 border border-gray-50" >
             <h3 className="text-xl text-gray-600 font-semibold" >{props.item.title}</h3>
-            <p className="text-gray-500 text-lg mt-2">{ props.item.description}</p>
+            <div className="flex flex-left">
+                <small className="text-sm text-blue-300 mx-2 font-semibold">{props.item.user.name}</small>
+                <small className="text-sm text-blue-300 mx-2 font-semibold">{props.item.user.name}</small>
+            </div>
+            <p className="text-gray-500 text-lg mt-2">{props.item.description}</p>
         </div>
     )
 }
