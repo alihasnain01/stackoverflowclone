fdf49718e70ac17ff5d1a1ab717756baaad1e8c6
unknown <ali.hasnain@redsignal.biz>
2023-11-22 17:12:31 +0500

    dropdown medue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 
 import { NavLink } from "react-router-dom";
 import { isLogin } from "../App";
-
+import HeaderDropDown from "./HeaderDropDown";
 const Header = () => {
+
     return (
-        <div className="overflow-hidden">
+        <>
             <nav className="w-full h-14 flex justify-between items-center px-4 md:px-32 bg-gray-700 top-0 sticky ">
                 <div className="text-gray-300 text-xl font-bold">Red Overflow</div>
                 <ul className="md:flex hidden font-semibold space-x-10 cursor-pointer text-gray-200">
@@ -20,17 +21,16 @@ const Header = () => {
                             <NavLink to="/login">  <div className="hidden md:block px-3 py-2 text-white cursor-pointer font-bold rounded border">Log in</div></NavLink>
                             <NavLink to="/signup"><div className="hidden md:block px-3 py-2 bg-indigo-700 text-white cursor-pointer font-bold rounded">Sign Up</div></NavLink>
                         </div>
-                        : <div className="flex items-center space-x-2">
-                            <p className="hidden md:block text-white font-semibold">Welcome, {isLogin.value.name} | </p>
-                            <p className="hidden md:block text-red-500 font-bold cursor-pointer" onClick={() => { isLogin.value = ''; }}>Logout</p>
-                        </div>
+                        : <HeaderDropDown />
                 }
+
+
 
                 <div className="md:hidden">
                     <button className="text-4xl" href="#">&#8801;</button>
                 </div>
             </nav >
-        </div>
+        </>
     )
 }
 
---
+++ src/components/HeaderDropDown.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { isLogin } from "../App";
+const HeaderDropDown = () => {
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!isDropdownOpen);
+    };
+
+    return (
+        <div className="flex items-center space-x-4">
+            <div className="relative">
+                <button
+                    className="flex items-center text-white focus:outline-none"
+                    onClick={toggleDropdown}
+                >
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/512/149/149071.png"  // Replace with your avatar image URL
+                        alt="Avatar"
+                        className="w-8 h-8 rounded-full mr-2"
+                    />
+                    <span className="hidden md:block"> {isLogin.value.name}</span>
+                </button>
+
+                {isDropdownOpen && (
+                    <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded shadow-lg">
+                        <button className="block px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer w-full text-left">
+                            Profile
+                        </button>
+                        <button className="block px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer w-full text-left">
+                            Settings
+                        </button>
+                        <button className="block px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer w-full text-left" onClick={() => { isLogin.value = ''; }}>
+                            Logout
+                        </button>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default HeaderDropDown;
