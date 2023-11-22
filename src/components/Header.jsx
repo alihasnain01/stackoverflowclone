526a453a50824dafde153a7bf7445fe6cdc553fa
unknown <ali.hasnain@redsignal.biz>
2023-11-22 16:27:25 +0500

    working header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,10 @@ const Header = () => {
             <nav className="w-full h-14 flex justify-between items-center px-4 md:px-32 bg-gray-700 top-0 sticky ">
                 <div className="text-gray-300 text-xl font-bold">Red Overflow</div>
                 <ul className="md:flex hidden font-semibold space-x-10 cursor-pointer text-gray-200">
-                    <NavLink to="/" className={({ isActive }) => isActive ? "text-gray-400 font-extrabold" : "text-gray-200 hover:text-gray-400"} >Home</NavLink>
-                    <NavLink to="/categories" className={({ isActive }) => isActive ? "text-gray-400 font-extrabold" : "text-gray-200 hover:text-gray-400"}>Categories</NavLink>
-                    <NavLink to="/users" className={({ isActive }) => isActive ? "text-gray-400 font-extrabold" : "text-gray-200 hover:text-gray-400"}>Users</NavLink>
-                    <NavLink to="/about-us" className={({ isActive }) => isActive ? "text-gray-400 font-extrabold" : "text-gray-200 hover:text-gray-400"}>About Us</NavLink>
+                    <NavLink to="/" className={({ isActive }) => isActive ? "text-gray-400 font-extrabold border-b-2 border-yellow-400 " : "text-gray-200 hover:text-gray-400"} >Home</NavLink>
+                    <NavLink to="/categories" className={({ isActive }) => isActive ? "text-gray-400 font-extrabold border-b-2 border-yellow-400" : "text-gray-200 hover:text-gray-400"}>Categories</NavLink>
+                    <NavLink to="/users" className={({ isActive }) => isActive ? "text-gray-400 font-extrabold border-b-2 border-yellow-400" : "text-gray-200 hover:text-gray-400"}>Users</NavLink>
+                    <NavLink to="/about-us" className={({ isActive }) => isActive ? "text-gray-400 font-extrabold border-b-2 border-yellow-400" : "text-gray-200 hover:text-gray-400"}>About Us</NavLink>
                 </ul>
 
                 {
@@ -27,7 +27,7 @@ const Header = () => {
                 }
 
                 <div className="md:hidden">
-                    <a className="text-4xl" href="#">&#8801;</a>
+                    <button className="text-4xl" href="#">&#8801;</button>
                 </div>
             </nav >
         </div>
